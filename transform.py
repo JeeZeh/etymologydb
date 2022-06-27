e6e584eb3413dcf5dfa40fa0f5f4014753c1272f
Jesse Ashmore <29103230+JeeZeh@users.noreply.github.com>
2022-06-27 01:12:54 +0100

    Combine download files

--- transform.py
+++ transform.py
@@ -3,32 +3,57 @@ import csv
 from zipfile import ZipFile
 import requests
 from tqdm import tqdm
+from zipfile import ZipFile
+import requests
+import io
 
-words_header = "id:ID,lang,text,:LABEL"
-relationships_header = ":START_ID,:TYPE,:END_ID"
+delim = "|"
 
-words: set[str] = set()
-relation: list[str] = []
+def strip_word(w: str):
+    return w.strip().replace(r"(''|\[\[|\]\]|\"|\|)", "").replace("[[", "").replace("]]", "").replace("\"", "").replace("|", "")
 
-print("Reading etymwn.csv")
-with open("import/etymwn.tsv", encoding="utf8") as source:
-    for line in tqdm(csv.reader(source, delimiter="\t"), unit="lines"):
-        words.add(line[0])
-        words.add(line[2].strip())
-        relations.append(",".join((line[0], line[1].split(":")[1], line[2])))
 
-print("Writing relationships.csv")
-with open("import/relationships.csv", mode="w+", encoding="utf8") as target_relationships:
-    target_relationships.write(relationships_header + "\n")
-    for relationship in tqdm(relations, unit="rels"):
-        target_relationships.write(relationship)
-        target_relationships.write("\n")
+def transform():
+    words_header = f"id:ID{delim}lang{delim}text{delim}:LABEL"
+    relationships_header = f":START_ID{delim}:TYPE{delim}:END_ID"
 
-print("Writing words.csv")
-with open("import/words.csv", mode="w+", encoding="utf8") as target_words:
-    target_words.write(words_header + "\n")
-    for w in tqdm(words, unit="words"):
-        target_words.write(",".join((w, w.replace(": ", ",", 1), "Word")))
-        target_words.write("\n")
+    words: set[str] = set()
 
-# neo4j-admin import --database neo4j --nodes "words-header.csv,words.csv" --relationships "relationships-header.csv,etymwn.csv"
+    print("Reading etymwn.csv and writing relationships.csv")
+    with open("import/relationships.csv", mode="w+", encoding="utf8") as target_relationships:
+        target_relationships.write(relationships_header + "\n")
+        with open("import/etymwn.tsv", encoding="utf8") as source:
+            for line in tqdm(csv.reader(source, delimiter="\t"), unit="lines"):
+                w1, w2 = strip_word(line[0]), strip_word(line[2])
+                words.add(w1)
+                words.add(w2)
+                target_relationships.write(f"{w1}{delim}{line[1][4:]}{delim}{w2}\n")
+
+    print("Writing words.csv")
+    with open("import/words.csv", mode="w+", encoding="utf8") as target_words:
+        target_words.write(words_header + "\n")
+        for w in tqdm(words, unit="words"):
+            target_words.write(f"{w}{delim}{w.replace(': ', delim, 1)}{delim}Word\n")
+
+
+# rm -rf /data/databases/neo4j
+# neo4j-admin import --database neo4j --nodes "/import/words.csv" --relationships "/import/relationships.csv" --delimiter="|"
+
+
+def download_etymwn():
+    etymwn_url = "http://etym.org/etymwn-20130208.zip"
+    print("Downloading", etymwn_url, "and extracting to import/")
+
+    # Download Etymwn
+    r = requests.get(
+        etymwn_url,
+        stream=True,
+    )
+    zip_as_bytes_io = io.BytesIO(r.content)
+    zip_file = ZipFile(zip_as_bytes_io)
+    zip_file.extractall("import/")
+
+
+if __name__ == "__main__":
+    download_etymwn()
+    transform()
